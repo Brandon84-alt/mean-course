c1818dc07d4d73caad4b7efa56d11a2e2e126262
Brandon84-alt <brandonlevis84@gmail.com>
2022-07-12 09:17:41 -0400

    Trying to connect mongodb cosmo instead mongoose
    mongoose -> cosmosdb

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,48 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require ("mongoose");
 const postsRoutes = require("./routes/posts");
-
+const { MongoClient } = require("mongodb");
 const app = express();
 
+//---------------------- Connection to MongoDB ----------------
+const cosmosDbUri = 'mongodb://ezshop-dev-admin:LUk95q9Ya6SkL1TvWurzx5swbN5dq3Y7RGhTrwEogLg1UAnUkpJ19qIEfIDn5wt0LBNqQrRKYGEex1qilmuPUg==@ezshop-dev-admin.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@ezshop-dev-admin@'
+const cosmosDbClient = new MongoClient(cosmosDbUri)
+
+
+console.log("Server connection to database...");
+   return (async () => {
+
+    await cosmosDbClient.connect()
+        .then((result)=> {
+            LogService.log("SUCC","Database", "init connection", "Connection to CosmosDb successfull.","")
+    }).catch((err)=> {
+        LogService.log("ERR","Database", "init connection", "Connection to CosmosDb failed.","")
+
+    });
+
+    productDb = cosmosDbClient.db('products')
+    productCollection = productDb.collection('product_schreter_demo')
+
+    usersDataDb = cosmosDbClient.db('user_data')
+    usersDescriptionsCollection = usersDataDb.collection('user_descriptions')
+
+    testDb = cosmosDbClient.db("Test");
+    shopifyCollection = testDb.collection("shopify");
+
+   });
+//---------------------- End of Connection to MongoDB ----------------
+
+
+//---------------------------------------- OLD DB
 mongoose.connect("mongodb+srv://Virati84:gAqJKTI4A23zqioB@cluster0.3ptpo.mongodb.net/node-angular?retryWrites=true&w=majority")
+
 .then(() => {
   console.log('Connected to database!');
 })
 .catch(()=>{
 console.log('Connection failed!');
 });
+//-------------------------------------------
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
